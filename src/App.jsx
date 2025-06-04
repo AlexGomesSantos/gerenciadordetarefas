e4b48f14ee045b5747fc3bb2f559418d913c77ed
Alex gomes santos <65431675+AlexGomesSantos@users.noreply.github.com>
2025-06-04 23:40:49 +0100

    adicionado os tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import Tasks from "./components/Tasks";
 import AddTasks from "./components/AddTasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar matemática",
+      description: "Estudar os assuntos de funções e limites",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar física",
+      description: "Estudar os assuntos de cinemática e dinâmica",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar química",
+      description: "Estudar os assuntos de átomos e moléculas",
+      isCompleted: false,
+    },
+  ]);
+
   return (
-    <div>
-      <h1>Gerenciador de tarefas</h1>
-      <AddTasks />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3x1 text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTasks />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 }
